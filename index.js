e5fcef88bf6cc265c74f45b1ba4fee2d6eed3ce6
Joonatan <joonatan.vuorela@gmail.com>
2018-07-27 16:32:29 +0300

    refactor code

--- index.js
+++ index.js
@@ -3,48 +3,48 @@ const romanNumberValues = require('./Values.js');
 const romanNumberTranslator = new romanNumberValues();
 
 
-//function that return number from roman numbers string
-function translateRomanNumber(str) {
-    const strlenght = str.length;
+    //function that return number from string which contains roman number
+    function translateRomanNumber(str) {
+        const strlenght = str.length;
 
-    let translation = 0;
-    let previousNumber = 0;
-    let currentNumber;
+        let translation = 0;
+        let previousNumber = 0;
+        let currentNumber;
 
-    for (let i = 0; i<strlenght;i++) {
-        currentNumber = romanNumberTranslator.translate(str[i]);
-        if (currentNumber === 0) {
-            return "ei validi syöte";
+        for (let i = 0; i<strlenght;i++) {
+            currentNumber = romanNumberTranslator.translate(str[i]);
+            if (currentNumber === 0) {
+                return "ei validi syöte";
+            }
+            
+            if (currentNumber <= previousNumber) {
+                translation += currentNumber;
+            } else {
+                translation -= previousNumber*2
+                translation += currentNumber;
+            }
+            
+            previousNumber = currentNumber;
+
         }
-        
-        if (currentNumber <= previousNumber) {
-            translation += currentNumber;
-        } else {
-            translation -= previousNumber*2
-            translation += currentNumber;
-        }
-        
-        previousNumber = currentNumber;
+
+        return translation;   
 
     }
 
-    return translation;   
 
-}
+    function run () {
 
-
-function run () {
-
-    console.log("Syötö roomalaisia numeroita ja saat palautuksena numeron kymmenjärjestelmästä")
-    let stdin = process.openStdin();
-    stdin.addListener("data", function(romanNumber) {
-    let translated = translateRomanNumber(romanNumber.toString().trim());
-    console.log("Syötit roomalaisen numeron " + 
-        romanNumber.toString().trim() + " ja se on käännettynä: " + translated);
-    console.log("Halutessasi syötä uusi roomalaine numero");
-  });
-    
-}
+        console.log("Syötä roomalaisia numeroita ja saat palautuksena numeron kymmenjärjestelmästä")
+        let stdin = process.openStdin();
+        stdin.addListener("data", function(romanNumber) {
+        let translated = translateRomanNumber(romanNumber.toString().trim());
+        console.log("Syötit roomalaisen numeron " + 
+            romanNumber.toString().trim() + " ja se on käännettynä: " + translated);
+        console.log("Halutessasi syötä uusi roomalaine numero");
+    });
+        
+    }
 
 
 run();
